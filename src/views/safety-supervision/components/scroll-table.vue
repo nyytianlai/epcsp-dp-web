5e64c92e9e6621bc77644bbf4a2af08a8cf24485
chenjiancong <chenjiancong@sutpc.com>
2023-04-19 16:28:35 +0800

    feat: 累计告警数据信息接口联调

--- src/views/safety-supervision/components/scroll-table.vue
+++ src/views/safety-supervision/components/scroll-table.vue
@@ -7,58 +7,77 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-      <ScrollTable
-        style="width: 100%; height: 3.44rem"
-        :autoScroll="false"
-        :interval="5000"
-        :data="gridInfo.data"
-        :highlight-current-row="false"
-        v-model="currentIndex"
-        @current-change="currentChange"
-        class="custom-table"
-      >
-        <el-table-column
-          v-for="(item, index) in gridInfo.columnKeyList"
-          :key="index"
-          :prop="item.prop"
-          align="left"
-          :label="item.label"
-          :min-width="item.width"
-        ></el-table-column>
-      </ScrollTable>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted,reactive } from 'vue'
-  import ScrollTable from '@sutpc/vue3-scroll-table';
+  <ScrollTable
+    style="width: 100%; height: 3.44rem"
+    :autoScroll="false"
+    :interval="5000"
+    :data="gridInfo.data"
+    :highlight-current-row="false"
+    v-model="currentIndex"
+    @current-change="currentChange"
+    class="custom-table"
+  >
+    <el-table-column
+      v-for="(item, index) in gridInfo.columnKeyList"
+      :key="index"
+      :prop="item.prop"
+      align="left"
+      :label="item.label"
+      :min-width="item.width"
+    ></el-table-column>
+  </ScrollTable>
+</template>
 
-  let gridInfo = reactive({
-    columnKeyList: [
-      { prop: 'pro', label: '运营商名称', width: 4 },
-      { prop: 'pro1', label: '告警数', width: 2 },
-      { prop: 'pro2', label: '已确认', width: 2 },
-      { prop: 'pro3', label: '恢复数', width: 2 }
-    ],
-    data: [
-        { pro: 'SG20220430sadfasdfasdfafds', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'asdfasdfasdfsadfasdfas', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-        { pro: 'SG20220430', pro1: '410', pro2: '400',pro3:'200' },
-    ]
-  });
-  let currentIndex = ref(0);
-  let currentChange = (row) => {
-    // 这里是选中的那一行数据
-  };
+<script setup lang="ts">
+import { ref, onMounted, reactive,watch } from 'vue';
+import ScrollTable from '@sutpc/vue3-scroll-table';
+
+const props = defineProps({
+  scrollTableData:{
+    type: Array,
+    default: ()=> {
+      return []
+    }
+  }
+})
+
+let gridInfo = reactive<any>({
+  columnKeyList: [
+    { prop: 'pro', label: '运营商名称', width: 4 },
+    { prop: 'pro1', label: '告警数', width: 2 },
+    { prop: 'pro2', label: '已确认', width: 2 },
+    { prop: 'pro3', label: '恢复数', width: 2 }
+  ],
+  data: [
+    { pro: 'SG20220430sadfasdfasdfafds', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'asdfasdfasdfsadfasdfas', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' },
+    { pro: 'SG20220430', pro1: '410', pro2: '400', pro3: '200' }
+  ]
+});
+let currentIndex = ref(0);
+let currentChange = (row) => {
+  // 这里是选中的那一行数据
+};
+
+watch(
+  ()=>props.scrollTableData,
+  ()=> {
+   gridInfo.data = props.scrollTableData
+  },
+  {
+    deep: true,
+    immediate: true
+  }
+  )
 </script>
 
-<style lang="less">
-</style>
+<style lang="less"></style>
--- src/views/safety-supervision/index.vue
+++ src/views/safety-supervision/index.vue
@@ -7,108 +7,141 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <page-num :data="pageNumData" bgcType="red-bgc" />
-    <panel>
-        <div class="total-warning-num">
-            <title-column title="累计告警数据信息" />
-            <tabs 
-            :data="totalWarningTabs" 
-            @changeTab="(data)=>handleChangeTab(data,'total-warning')" />
-            <scroll-table/>
-
-        </div>
-        <div class="area-warning-num">
-            <title-column title="行政区告警数据" />
-            <area-rank-list 
-                :data="areaRankData" 
-                :totalNum="areaTotalNum" 
-                height="3.7rem"
-            />
-        </div>
-    </panel>
-    <panel type="right">
-        <div class="warning-monitor">
-            <title-column title="今日设备告警监控" />
-            <tabs :data="warningMonitorTabs" @changeTab="(data)=>handleChangeTab(data,'warning-monitor')" />
-            <pie-chart :data="warningMonitorPieData"/>
-        </div>
-        <div class="realtime-state">
-            <title-column title="实时状态情况" />
-            <tabs :data="realtimeStateTabs" @changeTab="(data)=>handleChangeTab(data,'realtime-state')" />
-                <div class="num-wrap">
-                    <template v-for="(item, index) in realtimeStateData" :key="index">
-                        <num-card :data="item" />
-                    </template>
-                </div>
-            </div>
-            <div class="realtime-trend">
-                <title-column title="实时告警趋势情况" />
-                <line-time-chart :data="realtimeTrend" :chartStyle="{height:'2.55rem'}" :colors="['#FF6B4B']"/>
-            </div>
-    </panel>
-    <bottom-menu-tabs :data="bottomTabsData"/>
-  </template>
+  <page-num :data="pageNumData" bgcType="red-bgc" />
+  <panel>
+    <div class="total-warning-num">
+      <title-column title="累计告警数据信息" />
+      <tabs
+        :data="totalWarningTabs"
+        @changeTab="(data) => handleChangeTab(data, 'total-warning')"
+      />
+      <scroll-table :scrollTableData="scrollTableData"/>
+    </div>
+    <div class="area-warning-num">
+      <title-column title="行政区告警数据" />
+      <area-rank-list :data="areaRankData" :totalNum="areaTotalNum" height="3.7rem" />
+    </div>
+  </panel>
+  <panel type="right">
+    <div class="warning-monitor">
+      <title-column title="今日设备告警监控" />
+      <tabs
+        :data="warningMonitorTabs"
+        @changeTab="(data) => handleChangeTab(data, 'warning-monitor')"
+      />
+      <pie-chart :data="warningMonitorPieData" />
+    </div>
+    <div class="realtime-state">
+      <title-column title="实时状态情况" />
+      <tabs
+        :data="realtimeStateTabs"
+        @changeTab="(data) => handleChangeTab(data, 'realtime-state')"
+      />
+      <div class="num-wrap">
+        <template v-for="(item, index) in realtimeStateData" :key="index">
+          <num-card :data="item" />
+        </template>
+      </div>
+    </div>
+    <div class="realtime-trend">
+      <title-column title="实时告警趋势情况" />
+      <line-time-chart
+        :data="realtimeTrend"
+        :chartStyle="{ height: '2.55rem' }"
+        :colors="['#FF6B4B']"
+      />
+    </div>
+  </panel>
+  <bottom-menu-tabs :data="bottomTabsData" />
+</template>
 <script setup>
 import { ref, onMounted } from 'vue';
-import ScrollTable from './components/scroll-table.vue'
+import ScrollTable from './components/scroll-table.vue';
 import {
-    pageNumFun,
-    totalWarningTabsFun,
-    areaRankDataFun,
-    warningMonitorTabsFun,
-    warningMonitorPieDataFun,
-    realtimeStateTabsFun,
-    realtimeStateDataFun,
+  pageNumFun,
+  totalWarningTabsFun,
+  areaRankDataFun,
+  warningMonitorTabsFun,
+  warningMonitorPieDataFun,
+  realtimeStateTabsFun,
+  realtimeStateDataFun,
   realtimeTrendFun,
   bottomTabDataFun
 } from './config.js';
-import { districtAlarmLevelStatics,safetySupervisionAccumulated } from "@/api/supervision.js";
+import {
+  districtAlarmLevelStatics,
+  safetySupervisionAccumulated,
+  districtAlarmStatics
+} from '@/api/supervision.js';
 
 // 头部累计数据
-const getDistrictAlarmLevelStatics = async() => {
+const getDistrictAlarmLevelStatics = async () => {
   let { data } = await districtAlarmLevelStatics();
-  console.log(data.data,"getDistrictAlarmLevelStatics")
-}
+  console.log(data.data, 'getDistrictAlarmLevelStatics');
+};
 const pageNumData = ref(pageNumFun());
 // 累计告警数据信息
-const totalWarningTabs = ref(totalWarningTabsFun())
-
-const getSafetySupervisionAccumulated = async ()=> {
-  let {data} = await safetySupervisionAccumulated()
-  console.log(data,"getSafetySupervisionAccumulated")
-}
+const totalWarningTabs = ref(totalWarningTabsFun());
+const scrollTableData = ref([])
+const getSafetySupervisionAccumulated = async (type) => {
+  let { data } = await safetySupervisionAccumulated(type);
+  let newData = data?.data.map(item => {
+    return {
+      ...item,
+      pro: type === 1 ? item.operatorName :  item.stationName,
+      pro1: item.cnt,
+      pro2: item.affirmCnt,
+      pro3: item.recCnt
+    }
+  })
+  scrollTableData.value = newData || []
+};
 
 //行政区告警数据
-const areaRankData = ref(areaRankDataFun())
-const areaTotalNum = ref(6399)
+// const areaRankData = ref(areaRankDataFun())
+const areaRankData = ref();
+const areaTotalNum = ref(6399);
+const getDistrictAlarmStatics = async () => {
+  let { data } = await districtAlarmStatics();
+  let newData = data?.data?.map((item) => {
+    return {
+      ...item,
+      unit: '次',
+      num: item.cnt,
+      name: item.areaName
+    };
+  });
+  areaRankData.value = newData || [];
+};
 //今日设备告警监控
-const warningMonitorTabs = ref(warningMonitorTabsFun())
-const warningMonitorPieData = ref(warningMonitorPieDataFun())
+const warningMonitorTabs = ref(warningMonitorTabsFun());
+const warningMonitorPieData = ref(warningMonitorPieDataFun());
 //实时状态情况
-const realtimeStateTabs = ref(realtimeStateTabsFun())
-const realtimeStateData = ref(realtimeStateDataFun())
+const realtimeStateTabs = ref(realtimeStateTabsFun());
+const realtimeStateData = ref(realtimeStateDataFun());
 // 实时告警趋势情况
-const realtimeTrend = ref(realtimeTrendFun())
+const realtimeTrend = ref(realtimeTrendFun());
 //底部button
 const bottomTabsData = ref(bottomTabDataFun());
 const handleChangeTab = (data, type) => {
   console.log(data, type);
   if (type === 'total-warning') {
     //累计告警数据信息
-    
+    getSafetySupervisionAccumulated(data.code)
   } else if (type === 'warning-monitor') {
     // 今日设备告警监控
     warningMonitorPieData.value = warningMonitorPieDataFun(data.code);
   } else if (type === 'realtime-state') {
     // 实时状态情况
-    realtimeStateData.value = realtimeStateDataFun(data.code)
+    realtimeStateData.value = realtimeStateDataFun(data.code);
   }
 };
 
-onMounted(()=> {
+onMounted(() => {
   // getDistrictAlarmLevelStatics()
-  getSafetySupervisionAccumulated()
-})
+  getSafetySupervisionAccumulated(1);
+  getDistrictAlarmStatics();
+});
 </script>
 
 <style lang="less" scoped>
