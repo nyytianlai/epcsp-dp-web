15934a94143776d128b05bc4a81d86877032e34c
xiang cao <caoxiang@sutpc.com>
2023-04-28 18:30:11 +0800

    update: 修改地图参数

--- src/views/station-detail/index.vue
+++ src/views/station-detail/index.vue
@@ -2,7 +2,7 @@
  * @Author: xiang cao caoxiang@sutpc.com
  * @Date: 2023-04-17 15:04:38
  * @LastEditors: xiang cao caoxiang@sutpc.com
- * @LastEditTime: 2023-04-28 15:28:41
+ * @LastEditTime: 2023-04-28 18:29:55
  * @FilePath: \epcsp-dp-web\src\views\station-detail\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -203,7 +203,7 @@ const backSz = () => {
   });
   bus.emit('hrBackSz');
 };
-useEmitt('AIRCITY_EVENT', async (e) => {
+useEmitt && useEmitt('AIRCITY_EVENT', async (e) => {
   if (e.Id?.includes('camera')) { 
     pileType.value = 'monitor'
     const data = JSON.parse(e.UserData)
