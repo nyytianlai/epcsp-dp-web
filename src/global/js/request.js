9f3c7b56a63b7a58cb32b2c06f8a596454f6cfcf
linshuyi <linshuyi@sutpc.com>
2023-05-15 15:35:45 +0800

    Update: 回退成非md5参数

--- src/global/js/request.js
+++ src/global/js/request.js
@@ -11,27 +11,27 @@ request.config({
   // 如果要自定义返回的拦截器，记得把它设置为false
   // interceptor: false
 });
-axios.interceptors.request.use(
-  (config) => {
-    //发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等，根据需求去添加
-    config.data&&(config.data = JSON.stringify(setObject(config.data)))
-    console.log('config',config)
-    // config.data&&console.log('md5',md5(config.data))
-    // config.data = JSON.stringify(config.data) //数据转化,也可以使用qs转换
-    return config
-  },
-  (error) => {
-    Promise.reject(error)
-  }
-)
-// 将参数转成md5
-function setObject(obj){
-  let temp = JSON.parse(obj)
-  for(let i in temp){
-    temp[i] = md5(temp[i])
-  }
-  return temp
-}
+// axios.interceptors.request.use(
+//   (config) => {
+//     //发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等，根据需求去添加
+//     config.data&&(config.data = JSON.stringify(setObject(config.data)))
+//     console.log('config',config)
+//     // config.data&&console.log('md5',md5(config.data))
+//     // config.data = JSON.stringify(config.data) //数据转化,也可以使用qs转换
+//     return config
+//   },
+//   (error) => {
+//     Promise.reject(error)
+//   }
+// )
+// // 将参数转成md5
+// function setObject(obj){
+//   let temp = JSON.parse(obj)
+//   for(let i in temp){
+//     temp[i] = md5(temp[i])
+//   }
+//   return temp
+// }
 // 允许业务自定义返回的拦截器
 // axios.interceptors.request.use();
 // axios.interceptors.response.use();
