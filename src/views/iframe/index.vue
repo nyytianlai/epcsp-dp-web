57c080708e6167f5be8a459932da392d854d41ab
zhoujiawen <zhoujiawen@sutpc.com>
2023-05-27 11:37:40 +0800

    fix:修改iframe地址

--- src/views/iframe/index.vue
+++ src/views/iframe/index.vue
@@ -4,7 +4,7 @@
       frameborder="0"
       v-show="showIframe"
       scrolling="no"
-      src="http://localhost:9109/"
+      src="http://10.10.2.63:9109/"
       :onload="handleIframeLoaded"
     ></iframe>
     <div class="charging-station" @click="routerChange('/overall/allView')"></div>
