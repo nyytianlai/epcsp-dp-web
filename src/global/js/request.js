e7f69ec90c5b2d39e1966e0d442131e241140ebd
zhoujiawen <zhoujiawen@sutpc.com>
2023-06-02 11:32:46 +0800

    fix:修改http错误响应码描述

--- src/global/js/request.js
+++ src/global/js/request.js
@@ -56,11 +56,11 @@ const err = (error) => {
     console.log('------异常响应------', error.response.status);
     switch (error.response.status) {
       case 403:
-        ElMessage.error('拒绝访问');
+        ElMessage.error({ message: '用户得到授权，但是访问是被禁止的' });
         break;
       case 500:
         console.log('------error.response------', error.response);
-        ElMessage.error(error.message);
+        ElMessage.error({ message: '服务器错误,请联系管理员' });
         break;
       case 510:
         console.log('------error.response------', error.response);
@@ -84,7 +84,7 @@ const err = (error) => {
         ElMessage.error('网络超时');
         break;
       case 401:
-        ElMessage.error('未授权，请重新登录');
+        ElMessage.error({ message: '用户没有权限（令牌、用户名、密码错误），请重新登录！' });
         store.removeTokens().then(() => {
           location.reload();
         });
