b4a482f8a5dfdeda7af13e91bcaf17252c6440a5
huangzuyu <huangzuyu@sutpc.com>
2024-08-22 17:58:12 +0800

    feat: 车网互动 品牌支持

--- src/views/special-scene/carnet-interaction/components/brand-support.vue
+++ src/views/special-scene/carnet-interaction/components/brand-support.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="adjust-trend-analysis">
+  <div class="brand-support">
     <title-column title="品牌支持" />
-    <div class="distributed-content" v-loading="loading"></div>
+    <div class="brand-support-content" v-loading="loading"></div>
   </div>
 </template>
 
@@ -43,39 +43,19 @@ onMounted(async () => {
 </script>
 
 <style scoped lang="less">
-.adjust-trend-analysis {
+.brand-support {
   width: 100%;
   height: 100%;
   display: flex;
   flex-flow: column nowrap;
   row-gap: 12px;
 
-  .distributed-content {
+  .brand-support-content {
     position: relative;
     flex: 1;
     min-height: 0;
     border-radius: 4px;
     background: url('../images/brand-support-bg.png') no-repeat;
-    background-size: 100% 100%;
-    // box-shadow: 0px 1px 14px 0px rgba(0, 0, 0, 0.04), inset 0px 0px 35px 0px rgba(41, 76, 179, 0.2);
-    // background: linear-gradient(
-    //   219.05deg,
-    //   rgba(37, 177, 255, 0.02) 16.882%,
-    //   rgba(37, 177, 255, 0.2) 100%
-    // );
-
-    .unit {
-      position: absolute;
-      left: 8px;
-      right: 4px;
-      top: 4px;
-      display: flex;
-      justify-content: space-between;
-      pointer-events: none;
-      font-size: 12px;
-      font-weight: 400;
-      color: #b4c0cc;
-    }
   }
 }
 </style>
