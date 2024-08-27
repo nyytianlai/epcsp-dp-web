e2e5d35c003a5efb0ebb67ee881f2f722f4e514e
niyayong@sutpc.com <niyayong@sutpc.com>
2024-08-27 16:59:50 +0800

    fix: 充电站柱状图

--- src/components/map-layer/rect-bar.vue
+++ src/components/map-layer/rect-bar.vue
@@ -4,29 +4,73 @@ import { inject, onMounted, onBeforeUnmount } from 'vue';
 import { getRectBar, getRectBarByStreet, requestGeojsonData } from './api.js';
 import bus from '@/utils/bus';
 import { getHtmlUrl } from '@/global/config/map';
+import { getPopupHtml } from '@/utils/index';
 
-const aircityObj = inject('aircityObj');
+const aircityObj = inject<any>('aircityObj');
 
 const { useEmitt } = aircityObj.value;
 let barPositionBak = [];
-let currentBar = '';
+let chartHover;
 
 useEmitt('AIRCITY_EVENT', async (e) => {
   // 编写自己的业务
   // console.log('事件监听', e);
   if (e.eventtype === 'MarkerCallBack') {
     let quName = e.ID?.split('-')[1];
-    if (e.Data === 'mouseover') {
-      //鼠标悬浮事件
-      // await aircityObj.value.acApi.marker.setPopupSize(e.ID,[200,290])
+
+    if (e.Data.includes('mouseover-')) {
+      const data = JSON.parse(e.Data.split('mouseover-')[1]);
       changeXzqhColor('qu-' + quName, [75 / 255, 222 / 255, 255 / 255, 0.6]);
-    } else if (e.Data === 'mouseout') {
-      // await aircityObj.value.acApi.marker.setPopupSize(e.ID,[80,190])
+      chartHover = true;
+      drawHoverBarMarker(data, true);
+    }
+
+    if (e.Data.includes('mouseout-')) {
+      const data = JSON.parse(e.Data.split('mouseout-')[1]);
       changeXzqhColor('qu-' + quName, [75 / 255, 222 / 255, 255 / 255, 0.0]);
+      chartHover = false;
+      drawHoverBarMarker(data, false);
     }
   }
 });
-
+const drawHoverBarMarker = async (data, show = false) => {
+  await aircityObj.value.acApi.marker.deleteByGroupId('bar-hover-pop');
+  if (!show || !data || !chartHover) return;
+  const contentHeight = +data.contentHeight;
+  const yMax = +data.yMax;
+  const mPopUpUrl = getPopupHtml({
+    usePopupHtml: true,
+    com: 'rect-bar-hover-pop',
+    params: {
+      value: JSON.stringify(data)
+    }
+  });
+  const marker = {
+    id: 'bar-hover-pop',
+    groupId: 'bar-hover-pop',
+    userData: JSON.stringify(data),
+    coordinate: data.coordinates,
+    anchors: [-0.5, 1], //锚点，设置Marker的整体偏移，取值规则和imageSize设置的宽高有关，图片的左上角会对准标注点的坐标位置。示例设置规则：x=-imageSize.width/2，y=imageSize.height
+    imageSize: [1, 1], //图片的尺寸
+    range: [1, 1000000], //可视范围
+    imagePath: `${getHtmlUrl()}/static/images/barEllipse.png`, //显示图片路径
+    useTextAnimation: false, //关闭文字展开动画效果 打开会影响效率
+    popupURL: mPopUpUrl,
+    popupBackgroundColor: [1.0, 1.0, 1.0, 1], //弹窗背景颜色
+    autoHidePopupWindow: false,
+    popupSize: [180, 160],
+    popupOffset: [-90, -contentHeight / 2 - 80], //弹窗偏移
+    autoHeight: true, // 自动判断下方是否有物体
+    displayMode: 2 //智能显示模式  开发过程中请根据业务需求判断使用四种显示模式,
+  };
+  await aircityObj.value.acApi.marker.add(marker);
+  aircityObj.value.acApi.marker.showPopupWindow(marker.id, async () => {
+    if (!chartHover) {
+      aircityObj.value.acApi.marker.hidePopupWindow(marker.id);
+      aircityObj.value.acApi.marker.deleteByGroupId('bar-hover-pop');
+    }
+  });
+};
 const getBarPositionByQuName = (quName: string) => {
   let quItem = barPositionBak.filter((item) => {
     return item.properties.QUNAME === quName;
@@ -53,40 +97,54 @@ const addBar = async (type: 'qu' | 'jd', streetId?: string) => {
     });
   }
   res1.features.forEach((item, index) => {
-    let countObj = res.filter((i) => {
-      return type === 'qu'
-        ? i.areaCode == item.properties.QUCODE
-        : i.streetId == item.properties.JDCODE;
-    });
-    let contentHeight = 160;
-
+    let countObj =
+      res.find((i) => {
+        return type === 'qu'
+          ? i.areaCode == item.properties.QUCODE
+          : i.streetId == item.properties.JDCODE;
+      }) || {};
+    // let contentHeight = 160;
+    const itemMax = countObj?.stationCount;
+    let contentHeight = Math.max((160 * itemMax) / yMax, 60);
     let idEnd = type === 'qu' ? item.properties.QUNAME : item.properties.JDNAME;
     let areaCode = type === 'qu' ? item.properties.QUCODE : item.properties.JDCODE + '';
 
     const getCountStr = (count) => {
       return count || '--';
     };
+    const oPopUpUrl = getPopupHtml({
+      usePopupHtml: true,
+      com: 'rect-bar',
+      params: {
+        value: JSON.stringify({ ...countObj, coordinates: item.geometry.coordinates }),
+        yMax,
+        contentHeight,
+        quName: idEnd,
+        areaCode
+      }
+    });
     let o = {
       id: 'rectBar1-' + idEnd,
       groupId: `rectBar`,
       userData: areaCode,
       coordinate: item.geometry.coordinates,
       anchors: [-41, 19], //锚点，设置Marker的整体偏移，取值规则和imageSize设置的宽高有关，图片的左上角会对准标注点的坐标位置。示例设置规则：x=-imageSize.width/2，y=imageSize.height
-      imageSize: [82, 38], //图片的尺寸
+      imageSize: [82, 19], //图片的尺寸
       range: [1, 1000000], //可视范围
       imagePath: `${getHtmlUrl()}/static/images/barEllipse.png`, //显示图片路径
       useTextAnimation: false, //关闭文字展开动画效果 打开会影响效率
-      popupURL: `${getHtmlUrl()}/static/html/rectBar.html?value=${getCountStr(
-        countObj[0].gunCount
-      )},${getCountStr(countObj[0].stationCount)},${getCountStr(
-        countObj[0].equipmentCount
-      )},${getCountStr(countObj[0].esCount)},${getCountStr(countObj[0].v2gCount)},${getCountStr(
-        countObj[0].superCount
-      )}&yMax=${yMax}&contentHeight=${contentHeight}&quName=${idEnd}&areaCode=${areaCode}`, //弹窗HTML链接
+      // popupURL: `${getHtmlUrl()}/static/html/rectBar.html?value=${getCountStr(
+      //   countObj.gunCount
+      // )},${getCountStr(countObj.stationCount)},${getCountStr(
+      //   countObj.equipmentCount
+      // )},${getCountStr(countObj.esCount)},${getCountStr(countObj.v2gCount)},${getCountStr(
+      //   countObj.superCount
+      // )}&yMax=${yMax}&contentHeight=${contentHeight}&quName=${idEnd}&areaCode=${areaCode}`, //弹窗HTML链接
+      popupURL: oPopUpUrl,
       autoHidePopupWindow: false,
-      popupSize: [200, contentHeight + 160],
-      popupOffset: [-125, -140], //弹窗偏移
-      autoHeight: false, // 自动判断下方是否有物体
+      popupSize: [60, contentHeight],
+      popupOffset: [-30 - 41, -contentHeight / 2 + 19], //弹窗偏移
+      autoHeight: true, // 自动判断下方是否有物体
       displayMode: 2, //智能显示模式  开发过程中请根据业务需求判断使用四种显示模式,
       priority: item.properties.PRIORITY
     };
---
+++ src/popup-views/rect-bar-hover-pop/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="pop-panel">
+    <div class="title">
+      <img src="./images/position.png" class="tooltip-img" />
+      <span class="title-word">{{ paramsValue.quName }}</span>
+      <div class="title-bg"></div>
+    </div>
+    <div class="content">
+      <li>
+        充电站
+        <div class="liRight">
+          <span class="num num3">
+            {{ paramsValue.stationCount || 0 }}
+          </span>
+          个
+        </div>
+      </li>
+      <li>
+        充电桩
+        <div class="liRight">
+          <sapn class="num num1">
+            {{ paramsValue.equipmentCount || 0 }}
+          </sapn>
+          个
+        </div>
+      </li>
+      <li>
+        充电枪
+        <div class="liRight">
+          <span class="num num2">{{ paramsValue.gunCount || 0 }}</span>
+          个
+        </div>
+      </li>
+      <li>
+        欧标站
+        <div class="liRight">
+          <span class="num num4">{{ paramsValue.esCount || 0 }}</span>
+          个
+        </div>
+      </li>
+      <li>
+        V2G站
+        <div class="liRight">
+          <span class="num num5">{{ paramsValue.v2gCount || 0 }}</span>
+          个
+        </div>
+      </li>
+      <li>
+        超充站
+        <div class="liRight">
+          <span class="num num5">{{ paramsValue.superCount || 0 }}</span>
+          个
+        </div>
+      </li>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getUrlParam } from '@sutpc/zebra';
+const paramsValue = JSON.parse(getUrlParam('value') || '{}');
+</script>
+
+<style scoped lang="less">
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+#pop-panel {
+  width: 100%;
+  height: 100%;
+}
+.tooltip-img {
+  margin-left: 6px;
+}
+.title {
+  position: relative;
+  height: 24px;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.5);
+}
+.title-word {
+  z-index: 99;
+  position: relative;
+  bottom: 5px;
+  /* left: 3PX; */
+}
+
+.title-bg {
+  clip-path: polygon(0 0, 79% 0, 100% 100%, 0% 100%);
+  position: relative;
+  top: -15px;
+  left: 0;
+  background: rgba(0, 255, 249, 1);
+  width: 70%;
+  height: 15px;
+  z-index: -10;
+}
+.content {
+  clip-path: polygon(90% 0, 100% 13%, 100% 100%, 10% 100%, 0 87%, 0 0);
+  background: rgba(0, 48, 71, 0.95);
+  box-shadow: inset 0px 0px 16px rgba(75, 223, 255, 1);
+  padding: 12px;
+}
+li {
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 24px;
+  color: rgba(255, 255, 255, 0.8);
+}
+.liRight {
+  display: flex;
+}
+.num {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin-right: 2px;
+  background: linear-gradient(180deg, #00f7ff -71.43%, #d5feff 16%, #00f7ff 96.43%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+</style>
